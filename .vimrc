880afa7933f37064562653c83627d583c0f14fde
kotfind <kotfind@yandex.ru>
2021-12-07 09:50:32 +0300

    Upd
    .vimrc: ycm + snips
    .alacritty.yml: scroll
    xmonad.hs: layouts

--- .vimrc
+++ .vimrc
@@ -6,12 +6,47 @@ set rtp+=~/.vim/bundle/Vundle.vim
 call vundle#begin()
 
 Plugin 'VundleVim/Vundle.vim'
+
 Plugin 'majutsushi/tagbar'
+
 Plugin 'craigemery/vim-autotag'
+
 Plugin 'lervag/vimtex'
+    let g:tex_flavor='latex'
+    let g:vimtex_view_method='zathura'
+    let g:vimtex_quickfix_mode=0
+    set conceallevel=2
+    let g:tex_conceal='abdmg'
+    hi Conceal ctermbg=none
+    " hi clear Conceal
+
 Plugin 'preservim/nerdtree'
+
 Plugin 'SirVer/ultisnips'
+    let g:UltiSnipsExpandTrigger = "<c-j>"
+    " let g:UltiSnipsJumpForwardTrigger = "<c-j>"
+    " let g:UltiSnipsJumpBackwardTrigger = "<c-k>"
+
 Plugin 'ycm-core/YouCompleteMe'
+    let g:ycm_show_diagnostics_ui = 0
+    let g:ycm_confirm_extra_conf = 0
+    let g:ycm_autoclose_preview_window_after_insertion = 1
+    let g:ycm_autoclose_preview_window_after_completion = 1
+    let g:ycm_min_num_of_chars_for_completion=1
+    " python3 ~/.vim/bundle/YouCompleteMe/install.py --clang-completer
+    fun! ToggleYCMAutoComplete()
+        if !exists('g:ycm_auto_trigger')
+            return
+        endif
+
+        if g:ycm_auto_trigger
+            let g:ycm_auto_trigger=0
+        else
+            let g:ycm_auto_trigger=1
+        endif
+    endfun
+
+    nnoremap <F8> :call ToggleYCMAutoComplete()<CR>
 
 call vundle#end()
 filetype plugin indent on
@@ -97,23 +132,3 @@ noremap gwh <C-w>h
 noremap gwj <C-w>j
 noremap gwk <C-w>k
 noremap gwl <C-w>l
-
-" YCM
-let g:ycm_show_diagnostics_ui = 0
-let g:ycm_confirm_extra_conf = 0
-let g:ycm_autoclose_preview_window_after_insertion = 1
-let g:ycm_autoclose_preview_window_after_completion = 1
-let g:ycm_min_num_of_chars_for_completion=1
-" python3 ~/.vim/bundle/YouCompleteMe/install.py --clang-completer
-
-" vimtex
-let g:tex_flavor='latex'
-let g:vimtex_view_method='zathura'
-let g:vimtex_quickfix_mode=0
-hi clear Conceal
-
-" UltiSnips
-inoremap <c-n> <Nop>
-let g:UltiSnipsExpandTrigger="<tab>"
-let g:UltiSnipsJumpForwardTrigger="<c-n>"
-let g:UltiSnipsJumpBackwardTrigger="<c-b>"
--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -4,21 +4,48 @@ import XMonad.Hooks.SetWMName
 import XMonad.Actions.GridSelect
 import XMonad.Util.EZConfig
 import XMonad.Util.Ungrab
+import XMonad.Util.Run
+import XMonad.StackSet
+import XMonad.Layout.Dwindle
+import XMonad.Hooks.EwmhDesktops
+import XMonad.Layout.NoBorders
+import XMonad.Hooks.ManageHelpers
+import XMonad.Actions.CopyWindow
 
-defaults = def
+
+
+main = xmonad $ ewmh def
     { terminal              = my_terminal
     , modMask               = mod4Mask
     , handleEventHook       = fullscreenEventHook
+    , layoutHook            = myLayout
     , startupHook           = startup
     , borderWidth           = 2
     , normalBorderColor     = "black"
     , focusedBorderColor    = "orange"   
+    , manageHook            = myManageHook
     } `removeKeysP` myRemoveKeysP `additionalKeysP` myAdditionalKeysP
 
 startup = do
     setWMName "LG3D"
     spawn "xkbcomp ~/.config/xkb/my $DISPLAY > /dev/null 2>&1"
     spawn "xscreensaver --no-splash"
+    spawn "pgrep stalonetray > /dev/null || stalonetray"
+
+myLayout = 
+    tiled
+    ||| noBorders Full
+    ||| Spiral L XMonad.Layout.Dwindle.CW (1/1) (11/10) 
+  where
+    tiled    = Tall nmaster delta ratio
+    nmaster  = 1
+    ratio    = 1/2
+    delta    = 3/100
+
+myManageHook = composeAll
+    [ className =? "stalonetray"    --> doIgnore <+> doF copyToAll <+> doF swapUp
+    , isDialog                      --> doFloat
+    ]
 
 my_terminal :: String
 my_terminal = "alacritty"
@@ -27,16 +54,18 @@ scrot_dir :: String
 scrot_dir = "~/Pictures/"
 
 scrotCmd :: String -> String
-scrotCmd flags = "scrot '%s.png' " ++ flags ++ " -e 'mv $f " ++ scrot_dir ++ "' | tr -d '\n' | xclip -selection clipboard"
-myRemoveKeysP = [ "M-S-<Return>" ]
+scrotCmd flags = "scrot '%s.png' " ++ flags ++ " -e 'xclip -selection clipboard -t image/png \"$f\"; mv $f " ++ scrot_dir ++ "'"
+
+myRemoveKeysP = []
 
 myAdditionalKeysP =
     [ ("M-<Return>", spawn my_terminal)
-    , ("M-s", spawnSelected def ["telegram-desktop", "vk"])
-    , ("M-S-z", spawn "xscreensaver-command -lock")
+    , ("M-S-<Return>", windows swapMaster)
+    , ("M-p", spawn "rofi -combi-modi window,run,drun -show combi -modi combi")
+    , ("M-d", spawnSelected def ["telegram-desktop", "vk"])
+    , ("M-S-z", spawn "xscreensaver-command -lock && systemctl suspend")
+    , ("M-z", spawn "xscreensaver-command -lock")
     , ("<Print>",   unGrab *> spawn (scrotCmd ""))
     , ("C-<Print>", unGrab *> spawn (scrotCmd "-u -f"))
     , ("S-<Print>", unGrab *> spawn (scrotCmd "-s -f"))
     ]
-
-main = xmonad defaults
