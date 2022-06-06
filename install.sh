c377ed3611bb2cc26046a560daedb3131ec25887
kotfind <kotfind@yandex.ru>
2022-06-06 23:58:39 +0300

    install.sh: Upd

--- install.sh
+++ install.sh
@@ -12,3 +12,11 @@ find . \
     -exec echo Linking \{\} \; \
     -exec ln -sf $(readlink -f \{\}) ~/\{\} \;
 
+# Oh-My-Zsh Plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+
+# Vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+mkdir -p ~/.vim/undodir
+echo "Run :PluginInstall in vim"
