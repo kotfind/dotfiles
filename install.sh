2d2b0fe7b1d228d0b7464f2af9f5a9df9920c820
kotfind <kotfind@yandex.ru>
2022-06-07 00:22:49 +0000

    install.sh: BagFix

--- install.sh
+++ install.sh
@@ -1,10 +1,22 @@
-# Install zsh, oh-my-zsh and powerlevel10k
-pacman -S --needed zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-sudo -u $USER yay -S --needed zsh-theme-powerlevel10k ttf-meslo-nerd-font-powerlevel10k
+#!/bin/bash
 
-# Install graphics (with xmonad, alacrity and rofi)
-pacman -S --needed xorg-server xorg-init xmonad xmonad-contrib rofi alacritty xorg-xkbutils
+# Check if packages installed
+
+NEEDED_PACKAGES="zsh \
+    xorg-server xorg-xinit \
+    xmonad xmonad-contrib rofi stalonetray xscreensaver scrot\
+    alacritty xorg-xkbutils \
+    python3 cmake \
+    zsh-theme-powerlevel10k ttf-meslo-nerd-font-powerlevel10k"
+
+pacman -Q $NEEDED_PACKAGES &> /dev/null
+if ! [ $? -eq 0 ]; then
+    echo -e "Error: All these packages should be installed:\n$NEEDED_PACKAGES"
+    exit 1
+fi
+
+# Install zsh, oh-my-zsh and powerlevel10k
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh) --unattended"
 
 find . \
     -not -path "./.git*" \
@@ -28,5 +40,4 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.oh-my-zsh/cust
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 mkdir -p ~/.vim/undodir
 vim -c ":PluginInstall" -c ":qa"
-sudo pacman -S --needed python3 cmake
 python3 ~/.vim/bundle/YouCompleteMe/install.py --clang-completer
