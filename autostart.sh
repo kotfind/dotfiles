66644a8a4ac5b1a779d04fbdcf3c060af7abfc2e
kotfind <kotfind@yandex.ru>
2023-05-11 20:39:38 +0300

    Add autostart.sh

---
+++ autostart.sh
@@ -0,0 +1,11 @@
+run() {
+    pgrep -f $1 &> /dev/null || $* &> /dev/null &
+}
+
+xkbcomp ~/.config/xkb/my $DISPLAY > /dev/null 2>&1
+xrdb ~/.Xresources
+run slstatus
+run xss-lock slock
+run batsignal
+run /usr/lib/notification-daemon-1.0/notification-daemon
+run lxqt-policykit-agent
