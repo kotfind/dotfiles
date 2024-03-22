7157dda846c7a50a8f3f7b1a30d5493fad6104bb
kotfind <kotfind@yandex.ru>
2024-03-22 11:13:41 +0300

    Run lxqt-policykit-agent in autostart.sh

--- autostart.sh
+++ autostart.sh
@@ -10,6 +10,6 @@ xrdb ~/.Xresources
 run xss-lock slock
 run batsignal
 run /usr/lib/notification-daemon-1.0/notification-daemon
-# run lxqt-policykit-agent
+run lxqt-policykit-agent
 
 mkdir -p /tmp/TmpDownloads /tmp/TmpScreenshots
