9e779b736a47e11eaf7352417db9b2b772abd2b8
kotfind <kotfind@yandex.ru>
2024-07-12 22:14:56 +0300

    Configure lualine

--- nvim/.config/nvim/lua/plugins/looks.lua
+++ nvim/.config/nvim/lua/plugins/looks.lua
@@ -1,22 +1,66 @@
+local function setup_cyberdream()
+    require 'cyberdream'.setup {
+        italic_comments = true,
+        borderless_telescope = false,
+        theme = {
+            highlights = {
+                -- TreesitterContextBottom = { underline = true },
+            },
+        },
+    }
+
+    vim.cmd [[ colorscheme cyberdream ]]
+
+    -- toggle theme
+    Map('n', '<leader>T', ':CyberdreamToggleMode<CR>')
+end
+
+local function setup_lualine()
+    require 'lualine'.setup {
+        options = {
+            theme = require 'lualine.themes.cyberdream',
+        },
+        sections = {
+            lualine_a = {
+                {
+                    'filename',
+                    newfile_status = true,
+                    path = 1
+                }
+            },
+            lualine_b = { 'branch' },
+            lualine_c = {},
+            lualine_x = {
+                {
+                    'diagnostics',
+                    update_in_insert = true,
+                }
+            },
+            lualine_y = { 'filetype' },
+            lualine_z = { 'progress' },
+        },
+
+        inactive_sections = {
+            lualine_a = {
+                {
+                    'filename',
+                    newfile_status = true,
+                    path = 1
+                }
+            },
+            lualine_b = {},
+            lualine_c = {},
+            lualine_x = {},
+            lualine_y = {},
+            lualine_z = {}
+        },
+    }
+end
+
 return {
     {
         'scottmckendry/cyberdream.nvim',
-        config = function()
-            require 'cyberdream'.setup {
-                italic_comments = true,
-                borderless_telescope = false,
-                theme = {
-                    highlights = {
-                        -- TreesitterContextBottom = { underline = true },
-                    },
-                },
-            }
-
-            vim.cmd [[ colorscheme cyberdream ]]
-
-            -- toggle theme
-            Map('n', '<leader>T', ':CyberdreamToggleMode<CR>')
-        end
+        config = setup_cyberdream,
     },
 
     {
@@ -26,4 +70,12 @@ return {
             vim.o.updatetime = 10
         end
     },
+
+    {
+        'nvim-lualine/lualine.nvim',
+        config = setup_lualine,
+        dependencies = {
+            'scottmckendry/cyberdream.nvim',
+        },
+    }
 }
