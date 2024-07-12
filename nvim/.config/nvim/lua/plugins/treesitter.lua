738014bf2d4cc57383e3829690d45056b2f1465e
kotfind <kotfind@yandex.ru>
2024-07-12 21:25:35 +0300

    Add treesitter

---
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,52 @@
+local function setup_treesitter()
+    require 'nvim-treesitter.configs'.setup {
+        ensure_installed = {
+            'bash',
+            'c',
+            'cpp',
+            'diff',
+            'html',
+            'css',
+            'javascript',
+            'json',
+            'lua',
+            'markdown',
+            'markdown_inline',
+            'python',
+            'query',
+            'toml',
+            'vim',
+            'vimdoc',
+            'xml',
+            'yaml',
+            'rust',
+            'nasm',
+            'typst',
+            'toml',
+            'gitcommit',
+        },
+
+        auto_install = true,
+
+        highlight = {
+            enable = true,
+        },
+
+        playground = {
+            enable = true,
+        },
+
+        indent = {
+            enable = true,
+        },
+
+    }
+end
+
+return {
+    {
+        'nvim-treesitter/nvim-treesitter',
+        build = ':TSUpdate',
+        config = setup_treesitter,
+    },
+}
