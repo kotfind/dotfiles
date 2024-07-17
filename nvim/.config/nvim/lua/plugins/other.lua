d64c4e716760eb0fe9895c21041c18ca24931d72
kotfind <kotfind@yandex.ru>
2024-07-17 14:46:03 +0300

    Add Comment & ts_context_commentstring

--- nvim/.config/nvim/lua/plugins/other.lua
+++ nvim/.config/nvim/lua/plugins/other.lua
@@ -13,11 +13,47 @@ local function setup_ssr()
     Map({ 'n', 'x' }, '<leader>ss', ssr.open)
 end
 
+local function setup_comment()
+    require 'ts_context_commentstring'.setup {
+        enable_autocmd = false,
+    }
+
+    require 'Comment'.setup {
+        pre_hook = require 'ts_context_commentstring.integrations.comment_nvim'.create_pre_hook(),
+        toggler = {
+            line = '<leader>/',
+            block = '<leader>?',
+        },
+        opleader = {
+            line = '<leader>/',
+            block = '<leader>?',
+        },
+        extra = {
+            above = '<leader>/O',
+            below = '<leader>/o',
+            eol = '<leader>/A',
+        },
+    }
+end
+
 return {
+    -- Surround with braces
     { 'kylechui/nvim-surround', opts = {} },
     { 'm4xshen/autoclose.nvim', opts = {} },
+
+    -- Substitute
     {
         'cshuaimin/ssr.nvim',
         config = setup_ssr,
+    },
+
+    -- Comment
+    'JoosepAlviste/nvim-ts-context-commentstring',
+    {
+        'numToStr/Comment.nvim',
+        config = setup_comment,
+        dependencies = {
+            'JoosepAlviste/nvim-ts-context-commentstring'
+        }
     },
 }
