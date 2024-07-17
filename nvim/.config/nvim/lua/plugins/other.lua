79d9db41fee42d742b30592773a22ed87d550671
kotfind <kotfind@yandex.ru>
2024-07-17 13:15:49 +0300

    Add ssr

--- nvim/.config/nvim/lua/plugins/other.lua
+++ nvim/.config/nvim/lua/plugins/other.lua
@@ -1,4 +1,23 @@
+local function setup_ssr()
+    local ssr = require 'ssr'
+    ssr.setup {
+        keymaps = {
+            close = 'q',
+            next_match = 'n',
+            prev_match = 'N',
+            replace_confirm = '<cr>',
+            replace_all = '<leader><cr>',
+        },
+    }
+
+    Map({ 'n', 'x' }, '<leader>ss', ssr.open)
+end
+
 return {
     { 'kylechui/nvim-surround', opts = {} },
     { 'm4xshen/autoclose.nvim', opts = {} },
+    {
+        'cshuaimin/ssr.nvim',
+        config = setup_ssr,
+    },
 }
