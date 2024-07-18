d8f0802ee83a9189541d8c36e603e4d2007dd714
kotfind <kotfind@yandex.ru>
2024-07-18 11:51:11 +0300

    Add luasnip

--- nvim/.config/nvim/lua/plugins/cmp.lua
+++ nvim/.config/nvim/lua/plugins/cmp.lua
@@ -1,31 +1,48 @@
 local function setup_normal()
     local cmp = require 'cmp'
+    local luasnip = require 'luasnip'
 
     cmp.setup {
-        snippet = {},
+        snippet = {
+            expand = function(args)
+                luasnip.lsp_expand(args.body)
+            end
+        },
 
         mapping = cmp.mapping.preset.insert {
-            ['<CR>'] = cmp.mapping.confirm({ select = true }),
+            ['<CR>'] = cmp.mapping(function(fallback)
+                if cmp.visible() then
+                    if luasnip.expandable() then
+                        luasnip.expand()
+                    else
+                        cmp.confirm({
+                            select = true,
+                        })
+                    end
+                else
+                    fallback()
+                end
+            end),
 
-            ['<Tab>'] = cmp.mapping(function(fallback)
-                local col = vim.fn.col('.') - 1
-
+            ["<Tab>"] = cmp.mapping(function(fallback)
                 if cmp.visible() then
                     cmp.select_next_item()
-                elseif col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
-                    fallback()
+                elseif luasnip.locally_jumpable(1) then
+                    luasnip.jump(1)
                 else
-                    cmp.complete()
+                    fallback()
                 end
-            end, { 'i', 's' }),
+            end, { "i", "s" }),
 
-            ['<S-Tab>'] = cmp.mapping(function(fallback)
+            ["<S-Tab>"] = cmp.mapping(function(fallback)
                 if cmp.visible() then
                     cmp.select_prev_item()
+                elseif luasnip.locally_jumpable(-1) then
+                    luasnip.jump(-1)
                 else
                     fallback()
                 end
-            end, { 'i', 's' }),
+            end, { "i", "s" }),
         },
 
         formatting = {
@@ -46,6 +63,7 @@ local function setup_normal()
             { name = 'buffer' },
             { name = 'path' },
             { name = 'nvim-cmp-ts-tag-close' },
+            { name = 'luasnip' },
         }),
     }
 end
@@ -56,14 +74,13 @@ local function setup_cmdline()
     cmp.setup.cmdline(':', {
         mapping = cmp.mapping.preset.cmdline(),
         sources = cmp.config.sources({
-            { name = 'path' }
-        }, {
+            { name = 'path' },
             {
                 name = 'cmdline',
                 option = {
                     ignore_cmds = { 'Man', '!' }
                 }
-            }
+            },
         })
     })
 end
@@ -71,7 +88,7 @@ end
 local function setup_searchline()
     local cmp = require 'cmp'
 
-    cmp.setup.cmdline('/', {
+    cmp.setup.cmdline({ '/', '?' }, {
         mapping = cmp.mapping.preset.cmdline(),
         sources = {
             { name = 'buffer' },
@@ -94,9 +111,17 @@ return {
             'hrsh7th/cmp-buffer',
             'hrsh7th/cmp-path',
             'hrsh7th/cmp-cmdline',
-            'windwp/nvim-ts-autotag',
+            'buschco/nvim-cmp-ts-tag-close'
         },
     },
 
-    { 'windwp/nvim-ts-autotag', opts = {} },
+    {
+        'buschco/nvim-cmp-ts-tag-close',
+        opts = {}
+    },
+
+    {
+        'L3MON4D3/LuaSnip',
+        build = 'make install_jsregexp'
+    },
 }
