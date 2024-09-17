9136b45d57cea41d2abaecf323bce7fca7c31d7f
kotfind <kotfind@yandex.ru>
2024-09-17 18:33:20 +0300

    luasnip & some typst snips

--- nvim/.config/nvim/lua/plugins/cmp.lua
+++ nvim/.config/nvim/lua/plugins/cmp.lua
@@ -24,25 +24,21 @@ local function setup_normal()
                 end
             end),
 
-            ["<Tab>"] = cmp.mapping(function(fallback)
+            ['<Tab>'] = cmp.mapping(function(fallback)
                 if cmp.visible() then
                     cmp.select_next_item()
-                elseif luasnip.locally_jumpable(1) then
-                    luasnip.jump(1)
                 else
                     fallback()
                 end
-            end, { "i", "s" }),
+            end, { 'i', 's' }),
 
-            ["<S-Tab>"] = cmp.mapping(function(fallback)
+            ['<S-Tab>'] = cmp.mapping(function(fallback)
                 if cmp.visible() then
                     cmp.select_prev_item()
-                elseif luasnip.locally_jumpable(-1) then
-                    luasnip.jump(-1)
                 else
                     fallback()
                 end
-            end, { "i", "s" }),
+            end, { 'i', 's' }),
         },
 
         formatting = {
@@ -168,11 +164,9 @@ return {
             'hrsh7th/cmp-cmdline',
             'hrsh7th/cmp-calc',
             'f3fora/cmp-spell',
+            'saadparwaiz1/cmp_luasnip',
+            'L3MON4D3/cmp-luasnip-choice',
+            'L3MON4D3/LuaSnip',
         },
-    },
-
-    {
-        'L3MON4D3/LuaSnip',
-        build = 'make install_jsregexp'
     },
 }
---
+++ nvim/.config/nvim/lua/plugins/snippets.lua
@@ -0,0 +1,84 @@
+local function setup_snippets()
+    local ls = require 'luasnip'
+    local s = ls.snippet
+    local t = ls.text_node
+    local i = ls.insert_node
+    local f = ls.function_node
+    local d = ls.dynamic_node
+    local sn = ls.snippet_node
+    local fmt = require 'luasnip.extras.fmt'.fmt
+
+    ls.setup {}
+
+    Map('i', '<C-j>', function() ls.jump(1) end)
+    Map('i', '<C-k>', function() ls.jump(-1) end)
+
+    ls.add_snippets('typst', {
+        -- Figure TAB
+        s('ftab', fmt([[
+            #figure(
+                caption: [{caption}],
+                table(
+                    columns: {columns},
+                    table.header{header},
+                    {body}{print_pos}
+                )
+            )
+        ]], {
+            caption = i(1),
+            columns = i(2),
+            header = d(3, function(args)
+                local col_num = tonumber(args[1][1])
+                if col_num == nil or col_num <= 0 then
+                    return sn(nil, {})
+                end
+
+                local ans_nodes = {}
+                for col_id = 1, col_num do
+                    local new_nodes = fmt('[*{}*]', i(col_id))
+
+                    for _, node in ipairs(new_nodes) do
+                        table.insert(ans_nodes, node)
+                    end
+                end
+
+                return sn(nil, ans_nodes)
+            end, { 2 }),
+            body = d(4, function(args)
+                local col_num = tonumber(args[1][1])
+                if col_num == nil or col_num <= 0 then
+                    return sn(nil, {})
+                end
+
+                local ans_nodes = {}
+                for col_id = 1, col_num do
+                    local new_node = t('[],' .. (col_id ~= col_num and ' ' or ''))
+                    table.insert(ans_nodes, new_node)
+                end
+
+                return sn(nil, ans_nodes)
+            end, { 2 }),
+            print_pos = i(0),
+        })),
+
+        -- Figure IMage
+        s('fim', fmt([[
+            #figure(
+                caption: [{caption}],
+                image("{source}", height: {height})
+            )
+        ]], {
+            caption = i(1),
+            source = i(2),
+            height = i(3, "3cm"),
+        }))
+    })
+end
+
+return {
+    {
+        'L3MON4D3/LuaSnip',
+        build = 'make install_jsregexp',
+        config = setup_snippets
+    }
+}
