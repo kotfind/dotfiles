87c037c8d1164a9b93dbf775bd45bfbe918cab65
kotfind <kotfind@yandex.ru>
2024-12-29 15:12:58 +0300

    Fix autostart.sh

--- X11/autostart.sh
+++ X11/autostart.sh
@@ -4,10 +4,8 @@ run() {
     for pid in $(pgrep -f $1); do
         kill -9 "$pid"
     done
-    $*
+    $* &
 }
-
-SXHKD_SHELL=sh
 
 xkbcomp ~/.config/xkb/my $DISPLAY > /dev/null 2>&1
 
@@ -17,6 +15,8 @@ run xss-lock slock
 run batsignal
 run /usr/lib/notification-daemon-1.0/notification-daemon
 run lxqt-policykit-agent
+
+export SXHKD_SHELL=sh
 run sxhkd
 
 run ~/.config/lemonbar/lemonbar.sh
